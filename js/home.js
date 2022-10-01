b640a330a6c6b735321e79fb2887212725871780
Lazy <2211717435@qq.com>
2022-10-01 20:36:25 +0800

    Initial commit

---
+++ js/home.js
@@ -0,0 +1,59 @@
+//Typed.js
+var typed = new Typed('#hi', {
+    strings: [
+        "Lazy.",
+        "from Guangxi, China.",
+        "a student.",
+        "good at fish.",
+        "......?"
+    ],
+    typeSpeed: 100,
+    loop: true,
+});
+
+//Nav links
+function goHome() {
+    $("#home").addClass("nav-active");
+    $("#blog").removeClass("nav-active");
+    $("#projects").removeClass("nav-active");
+    $("#homeの").show();
+    $("#blogの").hide();
+    $("#projectsの").hide();
+}
+
+function goBlog() {
+    $("#blog").addClass("nav-active");
+    $("#home").removeClass("nav-active");
+    $("#projects").removeClass("nav-active");
+    $("#blogの").show();
+    $("#homeの").hide();
+    $("#projectsの").hide();
+
+    $.get("https://blog.imlazy.ink:233/index.php/feed/", function (data) {
+        $('#blogs').empty();
+        $(data).find('item').each(function () {
+            var $item = $(this);
+            var title = $item.find('title').text();
+            var link = $item.find('link').text();
+            //var description = $item.find('description').text();
+            //var pubDate = $item.find('pubDate').text();
+            var html = "<div class=\"box-item\">";
+            html += "<b>" + title + "</b>";
+            //html += "<em>" + timeString(pubDate) + "</em>";
+            //html += "<p>" + description + "</p>";
+            html += "&nbsp;&nbsp;->&nbsp;&nbsp;<a href=\"" + link + "\" target=\"_blank\">Read More</a>";
+            html += "</div>";
+            
+            $('#blogs').append(html);
+        });
+    });
+}
+
+function goProjects() {
+    $("#projects").addClass("nav-active");
+    $("#blog").removeClass("nav-active");
+    $("#home").removeClass("nav-active");
+    $("#projectsの").show();
+    $("#blogの").hide();
+    $("#homeの").hide();
+}
