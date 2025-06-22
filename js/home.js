02fd223b8a6d5abe20d9ac11f8f1b86482efb498
LazyCreeper <2211717435@qq.com>
2025-06-22 17:19:50 +0800

    feat: get blogs

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 const AppConfig = {
   data: () => ({
-    isShow: false
+    curC: 'homeの',
+    blogs: []
   }),
   methods: {
     initTyped() {
@@ -9,60 +10,29 @@ const AppConfig = {
         typeSpeed: 100,
         loop: true
       })
+    },
+
+    navShow(n) {
+      this.curC = n
+    },
+
+    async getBlog() {
+      const data = await (await fetch('https://blog.imlazy.ink:233/index.php/feed/')).text()
+
+      // 解析XML
+      const parser = new DOMParser()
+      const xmlDoc = parser.parseFromString(data, 'text/xml')
+      const items = xmlDoc.getElementsByTagName('item')
+      this.blogs = Array.from(items).map((item) => ({
+        title: item.getElementsByTagName('title')[0].textContent,
+        link: item.getElementsByTagName('link')[0].textContent
+      }))
     }
   },
   mounted() {
     this.initTyped()
+    this.getBlog()
   }
 }
 
 Vue.createApp(AppConfig).mount('#app')
-
-//Nav links
-function goHome() {
-  $('#home').addClass('nav-active')
-  $('#blog').removeClass('nav-active')
-  $('#projects').removeClass('nav-active')
-  $('#homeの').show()
-  $('#blogの').hide()
-  $('#projectsの').hide()
-}
-
-function goBlog() {
-  $('#blog').addClass('nav-active')
-  $('#home').removeClass('nav-active')
-  $('#projects').removeClass('nav-active')
-  $('#blogの').show()
-  $('#homeの').hide()
-  $('#projectsの').hide()
-
-  $.get('https://blog.imlazy.ink:233/index.php/feed/', function (data) {
-    $('#blogs').empty()
-    $(data)
-      .find('item')
-      .each(function () {
-        var $item = $(this)
-        var title = $item.find('title').text()
-        var link = $item.find('link').text()
-        //var description = $item.find('description').text();
-        //var pubDate = $item.find('pubDate').text();
-        var html = '<div class="box-item">'
-        html += '<b>' + title + '</b>'
-        //html += "<em>" + timeString(pubDate) + "</em>";
-        //html += "<p>" + description + "</p>";
-        html += '&nbsp;&nbsp;->&nbsp;&nbsp;<a href="' + link + '" target="_blank">Read More</a>'
-        html += '</div>'
-
-        $('#blogs').append(html)
-      })
-  })
-}
-
-function goProjects() {
-  $('#projects').addClass('nav-active')
-  $('#blog').removeClass('nav-active')
-  $('#home').removeClass('nav-active')
-  $('#projectsの').show()
-  $('#blogの').hide()
-  $('#homeの').hide()
-}
